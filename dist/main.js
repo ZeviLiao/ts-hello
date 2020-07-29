637d113947bbe6ba00c0fb9328c1eacfab2548ec
Zevi Liao <jointjm@gmail.com>
2020-07-30 04:34:39 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -1,30 +1,8 @@
-var hero = /** @class */ (function () {
-    function hero() {
-        var _this = this;
-        this._name = 'John Doe';
-        this.getSecretldentity1 = function () {
-            return _this._name;
-        };
-    }
-    hero.prototype.getSecretldentity = function () {
-        return this._name;
-    };
-    hero.prototype.getSecretldentity2 = function () {
-        // let self = this
-        // setTimeout(function () {
-        //   console.log(self._name + '2')
-        // }, 1000)
-        setTimeout(function () {
-            console.log(this._name + '2');
-        }.bind(this), 1000);
-    };
-    return hero;
-}());
-var h = new hero();
-// var stoleSecretldentity = hero.getSecretldentity.bind(hero);
-var stoleSecretldentity1 = h.getSecretldentity1;
-var stoleSecretldentity = h.getSecretldentity.bind(h);
-console.log(stoleSecretldentity1());
-console.log(stoleSecretldentity());
-console.log(h.getSecretldentity());
-console.log(h.getSecretldentity2());
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var point_1 = require("./point");
+var p = new point_1.Point(1, 2);
+p.draw();
+p.x = 12;
+var x = p.x;
+console.log(x);
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,7 @@
+import { Point } from './point'
 
-class hero {
-  _name = 'John Doe'
-  getSecretldentity1 = () => {
-    return this._name;
-  }
-  getSecretldentity() {
-    return this._name;
-  }
-
-  getSecretldentity2() {
-    // let self = this
-    // setTimeout(function () {
-    //   console.log(self._name + '2')
-    // }, 1000)
-    setTimeout(function () {
-      console.log(this._name + '2')
-    }.bind(this), 1000)
-    
-  }
-
-}
-
-let h = new hero()
-
-// var stoleSecretldentity = hero.getSecretldentity.bind(hero);
-var stoleSecretldentity1 = h.getSecretldentity1
-var stoleSecretldentity = h.getSecretldentity.bind(h)
-
-console.log(stoleSecretldentity1());
-console.log(stoleSecretldentity());
-console.log(h.getSecretldentity());
-console.log(h.getSecretldentity2());
-
-
+let p = new Point(1, 2)
+p.draw()
+p.x = 12
+let x = p.x
+console.log(x)
